8c990b30f638f34a6610bf8695dfc38997f08ac9
evilgeniusnyc <evilgeniusnyc@me.com>
2020-08-11 09:59:31 -0400

    getting better w/ 'pry'. git push exit ls pwd git -m Getting better with 'pry'. !

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,27 @@ def reload
   load 'config/environment.rb'
 end
 
+dick = Student.new("Dick")
+jane = Student.new("Jane")
+byron = Student.new("Byron")
+
+juan = Tutor.new("Juan")
+adriana = Tutor.new("Adriana")
+jorge = Tutor.new("Jorge")
+
+chemistry = Course.new("Chemistry", dick, juan)
+biology = Course.new("Biology", jane, adriana) 
+physics = Course.new("Physics", byron, jorge)
+
+#the correct way to connect students, tutors, and courses is to add
+#students and tutors to the course instance because students and tutors 
+#belong to the course.  
+
+# jane.tutors
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+
